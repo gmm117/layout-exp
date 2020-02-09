7f4506d14248828dba523f7005b17628bff8591c
hong <gmm117@naver.com>
2020-02-09 22:26:11 +0900

    Front-End Layouy 2 Complete

--- index.js
+++ index.js
@@ -4,17 +4,24 @@ import fe_layout2_html from './public/fe_layout2.html';
 let layout1 = fe_layout1_html;
 let layout2 = fe_layout2_html;
 let main_iframe = document.querySelector('#main_frame');
+let main_element = main_iframe.contentDocument || main_iframe.contentWindow.document;
 const fe_layout1 = document.querySelector('#fe_layout1');
 const fe_layout2 = document.querySelector('#fe_layout2');
 
-if(layout1) {
-    main_iframe.contentDocument.documentElement.innerHTML = layout1;
+
+function changeFrame(target) {
+    main_element.lastChild.style.display = "none";
+    main_element.lastChild.innerHTML = target;
+
+    setTimeout(function() {
+        main_element.lastChild.style.display = "block";
+    }, 100);
 }
 
-fe_layout1.addEventListener('click', function(e) {
-    main_iframe.contentDocument.documentElement.innerHTML = layout1;
+fe_layout1.addEventListener('click', (e) => {
+    changeFrame(layout1);
 });
 
 fe_layout2.addEventListener('click', (e) => {
-    main_iframe.contentDocument.documentElement.innerHTML = layout2;
+    changeFrame(layout2);
 });
